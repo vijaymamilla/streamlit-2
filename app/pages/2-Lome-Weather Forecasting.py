ff92d89aabb2ee5230ef815c74abbe3c04108fa9
Vijay Mamilla <vijay.mamilla@gmail.com>
2023-09-24 17:00:00 -0400

    Removed un wanted code.

--- app/pages/2-Lome-Weather Forecasting.py
+++ app/pages/2-Lome-Weather Forecasting.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -11,34 +10,34 @@ warnings.filterwarnings('ignore')
 tf.random.set_seed(42)
 np.random.seed(42)
 
+
 @st.cache_data
-def loadTestDf():
+def load_test_df():
     test_df = pd.read_csv('app/artifactory/lome_test.csv', index_col='date_time', parse_dates=True)
 
     return test_df
 
-class DataWindow():
-    def __init__(self, input_width, label_width, shift,
-                  test_df,
-                 label_columns=None):
+
+@st.cache_resource
+def build_lstm_model():
+    new_model = keras.models.load_model("app/artifactory/lstm_model.h5")
+
+    return new_model
 
 
+class DataWindow:
+    def __init__(self, input_width, label_width, shift, test_df, label_columns=None):
         self.test_df = test_df
-
         self.label_columns = label_columns
         if label_columns is not None:
             self.label_columns_indices = {name: i for i, name in enumerate(label_columns)}
         self.column_indices = {name: i for i, name in enumerate(test_df.columns)}
-
         self.input_width = input_width
         self.label_width = label_width
         self.shift = shift
-
         self.total_window_size = input_width + shift
-
         self.input_slice = slice(0, input_width)
         self.input_indices = np.arange(self.total_window_size)[self.input_slice]
-
         self.label_start = self.total_window_size - self.label_width
         self.labels_slice = slice(self.label_start, None)
         self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
@@ -75,32 +74,23 @@ class DataWindow():
         return self.make_dataset(self.test_df)
 
 
-@st.cache_resource
-def buildLstmModel():
-
-    new_model = keras.models.load_model("app/artifactory/lstm_model.h5")
-
-    return new_model
-
-
-def buildDataFrame(days:int):
-    custom_mo_wide_window = DataWindow(input_width=days, label_width=days, shift=days, test_df=loadTestDf(),
+def build_data_frame(days: int):
+    custom_mo_wide_window = DataWindow(input_width=days, label_width=days, shift=days, test_df=load_test_df(),
                                        label_columns=['precipitation_sum (mm)', 'rain_sum (mm)', 'river_discharge',
                                                       'intensity_rain', 'intensity_flood', 'intensity_drought'])
-    #input_indices = custom_mo_wide_window.input_indices
-    #label_indices = custom_mo_wide_window.label_indices
 
-    predicted_results = buildLstmModel().predict(custom_mo_wide_window.test)
+    predicted_results = build_lstm_model().predict(custom_mo_wide_window.test)
     predicted_array = predicted_results[0]
 
     my_array = np.array(predicted_array)
 
     df = pd.DataFrame(my_array)
 
-    df2 = df.rename(columns={0: "precipitation_sum (mm)", 1: "rain_sum (mm)", 2: "river_discharge", 3: "intensity_rain",4: "intensity_flood", 5: "intensity_drought"})
+    df2 = df.rename(columns={0: "precipitation_sum (mm)", 1: "rain_sum (mm)", 2: "river_discharge", 3: "intensity_rain",
+                             4: "intensity_flood", 5: "intensity_drought"})
 
     st.write("14 days forecast")
     st.dataframe(df2)
 
 
-buildDataFrame(14)
+build_data_frame(14)
