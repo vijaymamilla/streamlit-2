2edbaf50a44792c7f67954e4d1012151ec6e9bf3
Vijay Mamilla <vijay.mamilla@gmail.com>
2023-09-24 12:39:51 -0400

    Added lstm model

--- app/pages/2-Lome-Weather Forecasting.py
+++ app/pages/2-Lome-Weather Forecasting.py
@@ -1,47 +1,34 @@
-import pickle
-import numpy as np
-import pandas as pd
+
 import streamlit as st
-
-import tensorflow as tf
-
-print(tf.__version__)
-
-# In[60]:
-
-
-import datetime
-
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import matplotlib.pyplot as plt
-
-from tensorflow.keras import Model, Sequential
-
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.losses import MeanSquaredError
-from tensorflow.keras.metrics import MeanAbsoluteError
-
-from tensorflow.keras.layers import Dense, Conv1D, LSTM, Lambda, Reshape, RNN, LSTMCell
-
-
+from tensorflow import keras
 import warnings
 
 warnings.filterwarnings('ignore')
 
 tf.random.set_seed(42)
 np.random.seed(42)
+@st.cache_data
+def loadTrainDf():
+    train_df = pd.read_csv('app/artifactory/lome_train1.csv', index_col='date_time', parse_dates=True)
 
-train_df = pd.read_csv('app/artifactory/lome_train1.csv', index_col='date_time', parse_dates=True)
-val_df = pd.read_csv('app/artifactory/lome_val1.csv', index_col='date_time', parse_dates=True)
-test_df = pd.read_csv('app/artifactory/lome_test1.csv', index_col='date_time', parse_dates=True)
+    return train_df
+@st.cache_data
+def loadValDf():
+    val_df = pd.read_csv('app/artifactory/lome_val1.csv', index_col='date_time', parse_dates=True)
+    return val_df
+@st.cache_data
+def loadTestDf():
+    test_df = pd.read_csv('app/artifactory/lome_test1.csv', index_col='date_time', parse_dates=True)
+
+    return test_df
 
 
 class DataWindow():
     def __init__(self, input_width, label_width, shift,
-                 train_df=train_df, val_df=val_df, test_df=test_df,
+                 train_df, val_df, test_df,
                  label_columns=None):
 
         self.train_df = train_df
@@ -79,40 +66,6 @@ class DataWindow():
 
         return inputs, labels
 
-    def plot(self, model=None, plot_col='rain_sum (mm)', max_subplots=3):
-        inputs, labels = self.sample_batch
-
-        plt.figure(figsize=(12, 8))
-        plot_col_index = self.column_indices[plot_col]
-        max_n = min(max_subplots, len(inputs))
-
-        for n in range(max_n):
-            plt.subplot(3, 1, n + 1)
-            plt.ylabel(f'{plot_col} [scaled]')
-            plt.plot(self.input_indices, inputs[n, :, plot_col_index],
-                     label='Inputs', marker='.', zorder=-10)
-
-            if self.label_columns:
-                label_col_index = self.label_columns_indices.get(plot_col, None)
-            else:
-                label_col_index = plot_col_index
-
-            if label_col_index is None:
-                continue
-
-            plt.scatter(self.label_indices, labels[n, :, label_col_index],
-                        edgecolors='k', marker='s', label='Labels', c='green', s=64)
-            if model is not None:
-                predictions = model(inputs)
-                plt.scatter(self.label_indices, predictions[n, :, label_col_index],
-                            marker='X', edgecolors='k', label='Predictions',
-                            c='red', s=64)
-
-            if n == 0:
-                plt.legend()
-
-        plt.xlabel('Time (h)')
-
     def make_dataset(self, data):
         data = np.array(data, dtype=np.float32)
         ds = tf.keras.preprocessing.timeseries_dataset_from_array(
@@ -139,99 +92,33 @@ class DataWindow():
     def test(self):
         return self.make_dataset(self.test_df)
 
-    @property
-    def sample_batch(self):
-        result = getattr(self, '_sample_batch', None)
-        if result is None:
-            result = next(iter(self.train))
-            self._sample_batch = result
-        return result
-
-
-# In[66]:
-
-
-class Baseline(Model):
-    def __init__(self, label_index=None):
-        super().__init__()
-        self.label_index = label_index
-
-    def call(self, inputs):
-        if self.label_index is None:
-            return inputs
-
-        elif isinstance(self.label_index, list):
-            tensors = []
-            for index in self.label_index:
-                result = inputs[:, :, index]
-                result = result[:, :, tf.newaxis]
-                tensors.append(result)
-            return tf.concat(tensors, axis=-1)
-
-        result = inputs[:, :, self.label_index]
-        return result[:, :, tf.newaxis]
-
-
-
-mo_wide_window = DataWindow(input_width=14, label_width=14, shift=1,
-                            label_columns=['precipitation_sum (mm)', 'rain_sum (mm)', 'river_discharge',
-                                           'intensity_rain', 'intensity_flood', 'intensity_drought'])
-
-
-
-def compile_and_fit(model, window, patience=3, max_epochs=50):
-    early_stopping = EarlyStopping(monitor='val_loss',
-                                   patience=patience,
-                                   mode='min')
-
-    model.compile(loss=MeanSquaredError(),
-                  optimizer=Adam(),
-                  metrics=[MeanAbsoluteError()])
-
-    history = model.fit(window.train,epochs=max_epochs,validation_data=window.val,callbacks=[early_stopping])
-
-    return model
-@st.cache_resource
-def buildDenseModel():
-    mo_dense = Sequential([
-        Dense(units=64, activation='relu'),
-        Dense(units=64, activation='relu'),
-        Dense(units=6, activation='relu')
-    ])
-
-    history = compile_and_fit(mo_dense, mo_wide_window)
-
-    return mo_dense
 
 @st.cache_resource
 def buildLstmModel():
-    mo_lstm_model = Sequential([
-        LSTM(32, return_sequences=True),
-        Dense(units=6, activation='relu')
-    ])
 
-    history = compile_and_fit(mo_lstm_model, mo_wide_window)
+    new_model = keras.models.load_model("app/artifactory/lstm_model.h5")
 
-    return mo_lstm_model
+    return new_model
 
 
+def buildDataFrame(days:int):
+    custom_mo_wide_window = DataWindow(input_width=days, label_width=days, shift=days,train_df=loadTrainDf(), val_df=loadValDf(), test_df=loadTestDf(),
+                                       label_columns=['precipitation_sum (mm)', 'rain_sum (mm)', 'river_discharge',
+                                                      'intensity_rain', 'intensity_flood', 'intensity_drought'])
+    #input_indices = custom_mo_wide_window.input_indices
+    #label_indices = custom_mo_wide_window.label_indices
 
-# In[53]:
-custom_mo_wide_window = DataWindow(input_width=14, label_width=14, shift=14,
-                                   label_columns=['precipitation_sum (mm)', 'rain_sum (mm)', 'river_discharge',
-                                                  'intensity_rain', 'intensity_flood', 'intensity_drought'])
-#input_indices = custom_mo_wide_window.input_indices
-#label_indices = custom_mo_wide_window.label_indices
+    predicted_results = buildLstmModel().predict(custom_mo_wide_window.test)
+    predicted_array = predicted_results[0]
 
-predicted_results = buildLstmModel().predict(custom_mo_wide_window.test)
-predicted_array = predicted_results[0]
+    my_array = np.array(predicted_array)
 
-my_array = np.array(predicted_array)
+    df = pd.DataFrame(my_array)
 
-df = pd.DataFrame(my_array)
+    df2 = df.rename(columns={0: "precipitation_sum (mm)", 1: "rain_sum (mm)", 2: "river_discharge", 3: "intensity_rain",4: "intensity_flood", 5: "intensity_drought"})
 
-df2 = df.rename(columns={0: "precipitation_sum (mm)", 1: "rain_sum (mm)", 2: "river_discharge", 3: "intensity_rain",4: "intensity_flood", 5: "intensity_drought"})
+    st.write("14 days forecast")
+    st.dataframe(df2)
 
-#df2.head(14)
-st.write("14 days forecast")
-st.dataframe(df2)
+
+buildDataFrame(14)
